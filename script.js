90e5c437ccbc1493ab32c126ad77123a6c2eff40
AlexandraC0DE <alexandramgarciag@gmail.com>
2023-01-16 00:47:06 -0500

    Se resolvieron varios errores que habían en los botones

--- script.js
+++ script.js
@@ -1,103 +1,175 @@
-    document.addEventListener('DOMContentLoaded', function () {
-        let listaTareas = document.getElementById("listaTareas");
-        let nuevaTareaInput = document.getElementById("nuevaTarea");
-        let agregarBtn = document.getElementById("agregarBtn");
-        let tareas = JSON.parse(localStorage.getItem("tareas")) || [];
-        let editando = false;
+document.addEventListener('DOMContentLoaded', function () {
+    const listaTareas = document.getElementById("listaTareas");
+    const nuevaTareaInput = document.getElementById("nuevaTarea");
+    const agregarBtn = document.getElementById("agregarBtn");
+    let tareas = JSON.parse(localStorage.getItem("tareas")) || [];
+    let editando = false;
+    let tareaActual = "";
 
-        // Cargar tareas guardadas en el almacenamiento local al cargar la página
-        tareas.forEach(function (tarea) {
-            let li = crearListItem(tarea);
-            listaTareas.appendChild(li);
-        });
+    // Cargar tareas guardadas en el almacenamiento local al cargar la página
+    tareas.forEach(function (tarea) {
+        let li = crearListItem(tarea);
+        listaTareas.appendChild(li);
+    });
 
-        // Agregar tarea al presionar el botón o la tecla Enter
-        agregarBtn.addEventListener("click", agregarTarea);
-        nuevaTareaInput.addEventListener("keydown", function (event) {
-            if (event.keyCode === 13) {
-                agregarTarea();
-            }
-        });
+    // Agregar tarea al presionar el botón o la tecla Enter
+    agregarBtn.addEventListener("click", agregarTarea);
+    nuevaTareaInput.addEventListener("keydown", function (event) {
+        if (event.keyCode === 13) {
+            agregarTarea();
+        }
+    });
 
-        // Crea un elemento li con la tarea y los botones de eliminar y editar
-        function crearListItem(tarea) {
-            let li = document.createElement("li");
-            li.textContent = tarea;
+    // Crea un elemento li con la tarea y los botones de eliminar y editar
+    function crearListItem(tarea) {
+        let li = document.createElement("li");
+        li.textContent = tarea;
 
         // Agregar botón de eliminar tarea
-let btnEliminar = document.createElement("button");
-btnEliminar.innerHTML = "Eliminar";
-btnEliminar.addEventListener("click", function(){
-eliminarTarea(li, tarea);
-});
-li.appendChild(btnEliminar);
+        let btnEliminar = document.createElement("button");
+        btnEliminar.textContent = "Eliminar";
+        btnEliminar.addEventListener("click", function () {
+            eliminarTarea(li, tarea);
+        });
+        li.appendChild(btnEliminar);
 
-        // Agregar boton de editar tarea 
+        // Agregar boton de editar tarea
         let btnEditar = document.createElement("button");
-        btnEditar.innerHTML = "Editar";
-        btnEditar.addEventListener("click", function(){
+        btnEditar.textContent = "Editar";
+        btnEditar.addEventListener("click", function () {
             editarTarea(li, tarea);
         });
         li.appendChild(btnEditar);
-
         return li;
     }
 
     function agregarTarea() {
         let tarea = nuevaTareaInput.value;
-        if (tarea === "" || editando) {
+        if (tarea === "") {
+            alert("La tarea no puede estar vacía.");
+            return;
+        }
+        if (editando) {
+            alert("Debe finalizar la edición antes de agregar una nueva tarea");
+            return;
+        }
+        if (tareas.includes(tarea)) {
+            alert("Ya existe una tarea con ese nombre, ingrese uno diferente.");
             return;
         }
         // Agrega la tarea a la lista y al almacenamiento local
         tareas.push(tarea);
-        localStorage.setItem("tareas", JSON.stringify(tareas));
+        guardarTareas();
 
-        // Crea el elemento li y lo agrega a la lista de tareas en el HTML
+        // Crea el elemento li y lo agrega a la lista de tareas
         let li = crearListItem(tarea);
         listaTareas.appendChild(li);
+
+        // Limpia el input y enfoca en él
         nuevaTareaInput.value = "";
-        console.log("Tarea agregada: " + tarea);
+        nuevaTareaInput.focus();
+        editando = false;
     }
 
     function eliminarTarea(li, tarea) {
-        tareas.splice(tareas.indexOf(tarea), 1);
-        localStorage.setItem("tareas", JSON.stringify(tareas));
+        if (editando) {
+            alert("Debe finalizar la edición antes de eliminar una tarea.");
+            return;
+        }
+        // Elimina la tarea de la lista y del almacenamiento local
         listaTareas.removeChild(li);
-        console.log("Tarea eliminada: " + tarea);
+        tareas.splice(tareas.indexOf(tarea), 1);
+        guardarTareas();
     }
+
+    document.getElementById("formulario-editar-tarea").addEventListener("submit", guardarTareaEditada);
 
     function editarTarea(li, tarea) {
-        if(editando) return;
+        if (editando) {
+            alert("Debe finalizar la edición antes de comenzar otra.");
+            return;
+        }
+        //Activa el modo de edición
         editando = true;
-        let valorActual = li.firstChild.nodeValue;
-        let input = document.createElement("input");
-        input.type = "text";
-        input.value = valorActual;
-        li.replaceChild(input, li.firstChild);
-        //crear botón guardar
+        tareaActual = tarea;
+
+        function guardarTareaEditada(event) {
+            event.preventDefault();
+            //Actualizar la tarea en el almacenamiento local
+            tareas[tareas.indexOf(tareaActual)] = nuevaTareaInput.value;
+            guardarTareas();
+
+            //Actualizar la tarea en la lista
+            li.textContent = nuevaTareaInput.value;
+            nuevaTareaInput.value = "";
+            nuevaT
+        }
+
+        //Agrega un input de texto con el valor de la tarea Actual en el elemento li y oculta el contenido de texto
+        let inputEditar = document.createElement("input");
+        inputEditar.type = "text";
+        inputEditar.value = tarea;
+        li.textContent = "";
+        li.appendChild(inputEditar);
+
+        //Agrega un boton de guardar y cancelar
         let btnGuardar = document.createElement("button");
-        btnGuardar.innerHTML = "Guardar";
-        btnGuardar.addEventListener("click", function(){
-            guardarTarea(li, input, tarea);
+        btnGuardar.textContent = "Guardar";
+        btnGuardar.addEventListener("click", function () {
+            guardarEdicion(li, tarea);
         });
         li.appendChild(btnGuardar);
-
-        //agrega evento keydown para guardar al presionar enter
-        input.addEventListener("keydown", function (event) {
-            if (event.keyCode === 13) {
-                guardarTarea(li, input, tarea);
-            }
+        let btnCancelar = document.createElement("button");
+        btnCancelar.textContent = "Cancelar";
+        btnCancelar.addEventListener("click", function () {
+            cancelarEdicion(li, tarea);
         });
+        li.appendChild(btnCancelar);
+    }
+    function guardarEdicion(li, tarea) {
+        let nuevaTarea = li.firstChild.value;
+        if (nuevaTarea === "") {
+            alert("La tarea no puede estar vacía.");
+            return;
+        }
+        if (tareas.includes(nuevaTarea)) {
+            alert("Ya existe una tarea con ese nombre, ingrese uno diferente.");
+            return;
+        }
+        tareas[tareas.indexOf(tarea)] = nuevaTarea;
+        guardarTareas();
+        li.textContent = nuevaTarea;
+        li.appendChild(crearBtnEditar());
+        li.appendChild(crearBtnEliminar());
+        editando = false;
+    }
+    function cancelarEdicion(li, tarea) {
+        li.textContent = tarea;
+        li.appendChild(crearBtnEditar());
+        li.appendChild(crearBtnEliminar());
+        editando = false;
+    }
+    function crearBtnEditar() {
+        let btnEditar = document.createElement("button");
+        btnEditar.textContent = "Editar";
+        btnEditar.addEventListener("click", function () {
+            let tarea = this.parentNode.firstChild.data;
+            editarTarea(this.parentNode, tarea);
+        });
+        return btnEditar;
     }
 
-    function guardarTarea(li, input, tarea) {
-        let nuevaTarea = input.value;
-        tareas[tareas.indexOf(tarea)] = nuevaTarea;
-        localStorage.setItem("tareas", JSON.stringify(tareas));
-        li.replaceChild(document.createTextNode(nuevaTarea), input);
-        let btnGuardar = li.querySelector("button");
-        li.removeChild(btnGuardar);
-        editando = false;
-        console.log("Tarea guardada: " + nuevaTarea);
-        }
+    function crearBtnEliminar() {
+        let btnEliminar = document.createElement("button");
+        btnEliminar.textContent = "Eliminar";
+        btnEliminar.addEventListener("click", function () {
+            let tarea = this.parentNode.firstChild.data;
+            eliminarTarea(this.parentNode, tarea);
         });
+        return btnEliminar;
+    }
+
+    function guardarTareas() {
+        localStorage.setItem("tareas", JSON.stringify(tareas));
+    }
+});    
